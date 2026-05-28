c4a3f3233a5eb85e2bac4c43a7a51204a383267e
edgar.g <90957232+edgar-g-codexoptimus@users.noreply.github.com>
2026-05-28 21:20:56 +0300

    Убрал лишнюю зависимость

--- app/src/main/java/ru/edgarakert/biblenote/ui/screens/notes/NotesListScreen.kt
+++ app/src/main/java/ru/edgarakert/biblenote/ui/screens/notes/NotesListScreen.kt
@@ -48,7 +48,6 @@ import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.material3.rememberSwipeToDismissBoxState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
